0868cb11439f07631c438372a4cb02b7689b96f7
Harshitha-channamalla <harshithachannamalla@gmail.com>
2026-04-28 16:37:55 +0530

    Fix nested if-else structure for clarity

---
+++ nested if-else.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+   int a,b,c;
+   printf("enter the values of a,b,c :");
+   scanf("%d %d %d", &a, &b, &c);
+   if(a>b)
+   if (a>c)
+   printf ("%d is greater than",a);
+   else 
+   printf("%d is greater than ",c);
+   else
+   if(b>c)
+   printf ("%d is greater than",b);
+   else 
+   
+   printf ("%d is greater than", c);
+   
+    return 0;
+}
+output
+enter the values of a,b,c :66 7 89
+89 is greater than 
+
+=== Code Execution Successful ===
